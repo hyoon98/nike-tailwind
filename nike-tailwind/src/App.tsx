a5aabae147e6015bdf67dec16922086efe45a322
hyoon1998 <hya62@sfu.ca>
2023-11-14 01:28:06 -0800

    navbar

--- nike-tailwind/src/App.tsx
+++ nike-tailwind/src/App.tsx
@@ -1,29 +1,31 @@
+import {CustomerReviews, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality,Footer} from './sections/index'
+import Nav from './components/Nav.tsx';
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero/>
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts/>
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality/>
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services/>
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer/>
     </section>
     <section className="bg-pale=blue padding">
-      CustomerReviews
+      <CustomerReviews/>
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe/>
     </section>
     <section className="padding-x padding-t pb-8 bg-black">
-      Footer
+      <Footer/>
     </section>
   </main>
 )
---
+++ nike-tailwind/src/sections/index.tsx
@@ -0,0 +1,12 @@
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+import Hero from "./Hero";
+
+export{
+    Hero, PopularProducts, SuperQuality, Services, Footer, SpecialOffer, Subscribe, CustomerReviews
+}
