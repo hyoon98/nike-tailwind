4eb3c597312022532e3034932d573a2a9e892e24
hyoon1998 <hya62@sfu.ca>
2023-12-20 03:38:49 -0800

    finish

--- nike-tailwind/src/components/Button.tsx
+++ nike-tailwind/src/components/Button.tsx
@@ -4,13 +4,14 @@ type Props = {
   backgroundColor?: string,
   borderColor?: string,
   textColor?: string,
+  fullWidth?: boolean
 }
 
-const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }: Props) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }: Props) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border
         font-montserrat text-lg leading-none
-        ${backgroundColor? `${backgroundColor} ${textColor} ${borderColor}`:"bg-coral-red text-white border-coral-red"} rounded-full`}>
+        ${backgroundColor? `${backgroundColor} ${textColor} ${borderColor}`:"bg-coral-red text-white border-coral-red"} rounded-full ${fullWidth &&"w-full"}`}>
       {label}
       {iconURL && <img src={iconURL} alt="arrow right icon"
         className="ml-2 rounded-full w-5 h-5"
